5427f52c211cae2558b97865fac5896953395121
ThiagoModena <163160442+ThiagoModena@users.noreply.github.com>
2025-12-04 00:28:22 -0300

    Delete client directory

--- client/src/app/estheticians/models/esthetician.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Esthetician {
-  cpf: string;
-  first_name: string;
-  last_name: string;
-  specialty: string;
-}
--- client/src/app/procedures/models/procedure.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Procedure {
-  id: number; // Assumindo que o ID é um número e é retornado pela API na listagem e criação
-  name: string;
-  price: number;
-}
-
-
